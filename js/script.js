2997cb3819521e492381ba9639cee2d324ff5789
selimzade <wselimzade@mail.ru>
2023-05-17 16:45:35 +0400

    theme change tes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+"use strict";
+
+const ball = document.querySelector(".ball");
+const themeContent = document.querySelector(".theme-content");
+const toggleBall = () => {
+  ball.classList.toggle("light");
+  document.body.style.backgroundColor = "#F1F2F3";
+  themeContent.style.backgroundColor = "#ffffff";
+
+  if (!ball.classList.contains("light")) {
+    document.body.style.backgroundColor = "#17171C";
+    themeContent.style.backgroundColor = "#2E2F38";
+  }
+};
+ball.addEventListener("click", toggleBall);
 
