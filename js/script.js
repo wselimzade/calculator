42cceeba01313cc6cb73d112c44ade00d1128c3d
selimzade <wselimzade@mail.ru>
2023-05-18 11:35:59 +0400

    calc test

--- js/script.js
+++ js/script.js
@@ -26,7 +26,9 @@ const six = document.querySelector("#six");
 const seven = document.querySelector("#seven");
 const eight = document.querySelector("#eight");
 const nine = document.querySelector("#nine");
-const zero = document.querySelector("#zero")
+const zero = document.querySelector("#zero");
+
+const dot = document.querySelector("#dot")
 
 const clear = document.querySelector("#C");
 const del = document.querySelector("#delete");
@@ -38,86 +40,166 @@ const divide = document.querySelector("#divide");
 const multiply = document.querySelector("#multiply");
 const minus = document.querySelector("#minus");
 const plus = document.querySelector("#plus");
-const equal = document.querySelector("#equal")
+const equal = document.querySelector("#equal");
 
 const input = document.querySelector("#input");
 const result = document.querySelector("#result");
 
-one.addEventListener("click", () => {result.value += 1});
-two.addEventListener("click", () => {result.value += 2});
-three.addEventListener("click", () => {result.value += 3});
-four.addEventListener("click", () => {result.value += 4});
-five.addEventListener("click", () => {result.value += 5});
-six.addEventListener("click", () => {result.value += 6});
-seven.addEventListener("click", () => {result.value += 7});
-eight.addEventListener("click", () => {result.value += 8});
-nine.addEventListener("click", () => {result.value += 9});
-zero.addEventListener("click", () => {result.value += 0});
+let rhas = false;
+
+one.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "";
+    rhas = false;
+  }
+  result.value += 1;
+});
+two.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "";
+    rhas = false;
+  }
+  result.value += 2;
+});
+three.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "";
+    rhas = false;
+  }
+  result.value += 3;
+});
+four.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "";
+    rhas = false;
+  }
+  result.value += 4;
+});
+five.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "";
+    rhas = false;
+  }
+  result.value += 5;
+});
+six.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "";
+    rhas = false;
+  }
+  result.value += 6;
+});
+seven.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "";
+    rhas = false;
+  }
+  result.value += 7;
+});
+eight.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "";
+    rhas = false;
+  }
+  result.value += 8;
+});
+nine.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "";
+    rhas = false;
+  }
+  result.value += 9;
+});
+zero.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "";
+    rhas = false;
+  }
+  result.value += 0;
+});
+
+dot.addEventListener("click", () => {
+  if (rhas) {
+    result.value = "0."; // İlk ondalık basamağı eklemek için "0." değerini atayın
+    rhas = false;
+  } else if (result.value.indexOf(".") === -1) {
+    // Eğer sonuçta daha önce nokta yoksa
+    result.value += "."; // Noktayı ekle
+  }
+});
 
 let operator;
-// input.value = null
-// result.value = null
 
 divide.addEventListener("click", () => {
-
   input.value += result.value + divide.textContent;
-  result.value = null
+  result.value = null;
   operator = "/";
 });
 multiply.addEventListener("click", () => {
-  input.value += result.value +  multiply.textContent
-  result.value = null
+  input.value += result.value + multiply.textContent;
+  result.value = null;
   operator = "*";
 });
 minus.addEventListener("click", () => {
   input.value += result.value + minus.textContent;
-  result.value = null
+  result.value = null;
   operator = "-";
 });
-
 plus.addEventListener("click", () => {
-  input.value += result.value  + plus.textContent;
-  result.value = null
-  operator = "+"
+  input.value += result.value + plus.textContent;
+  result.value = null;
+  operator = "+";
 });
-remainder.addEventListener("click", ()=> {
+remainder.addEventListener("click", () => {
   input.value += result.value + remainder.textContent;
-  result.value = null
-  operator = "%"
-})
+  result.value = null;
+  operator = "%";
+});
 
 equal.addEventListener("click", () => {
-  let inputVal = parseInt(input.value);
-  let resultVal = parseInt(result.value);
- 
-  switch(operator) {
-    case "+": 
-        result.value = inputVal + resultVal;
-        input.value = result.value
-        break;
-    case "-": 
-        result.value = inputVal - resultVal;
-        break;
-    case "%": 
-        result.value = inputVal % resultVal;
-        break;
-    case "*": 
-        result.value = inputVal * resultVal;
-        break;
+  let inputVal = parseFloat(input.value);
+  let resultVal = parseFloat(result.value);
+  let value = resultVal;
+  let calculationResult;
+
+  switch (operator) {
+    case "+":
+      calculationResult = inputVal + resultVal;
+      break;
+    case "-":
+      calculationResult = inputVal - resultVal;
+      break;
+    case "%":
+      calculationResult = inputVal % resultVal;
+      break;
+    case "*":
+      calculationResult = (inputVal * resultVal);
+      break;
     case "/":
-        result.value = inputVal / resultVal;
-        break;
-    default: 
-        result.value = "Invalid operator";
+      calculationResult = inputVal / resultVal;
+      break;
+    default:
+      result.value = "Invalid operator";
   }
+
+  if (Number.isInteger(calculationResult)) {
+    result.value = calculationResult.toFixed(0);
+  } else {
+    result.value = calculationResult.toFixed(2);
+  }
+
+  input.value = "";
+  rhas = true;
 });
 
-clear.addEventListener("click", ()=> {input.value = ""; result.value = ""})
-del.addEventListener("click", () => { result.value = result.value.slice(0,-1)})
-negative.addEventListener("click", () => {result.value = "-" + result.value})
-
-const calc = () => {
-  
-}
-
-
+clear.addEventListener("click", () => {
+  input.value = "";
+  result.value = "";
+  rhas = false;
+});
+del.addEventListener("click", () => {
+  result.value = result.value.slice(0, -1);
+});
+negative.addEventListener("click", () => {
+  result.value = "-" + result.value;
+});
